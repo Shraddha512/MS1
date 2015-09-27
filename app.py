28ed102e4f60b5fd7785b1858b8e3281368559d1
unknown <shranaik@SHRANAIK-FLV7Z.cisco.com>
2015-09-27 18:46:34 -0400

    adding all files

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, json, request, g
+import sqlite3 as sql
+
+app = Flask(__name__)
+DATABASE = 'user_db.db'
+HOST = "0.0.0.0"
+PORT = 5000
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST', 'GET'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # validate the received values
+        if _name and _email and _password:
+            con = sql.connect("user_db.db")
+            cur = con.cursor()
+            cur.execute("INSERT INTO users (name,email, password) VALUES (?,?,?)", (_name,_email,_password))
+            con.commit()
+            con.close()
+            return render_template('signin.html')
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+    finally:
+        print("In finally")
+
+
+@app.route('/signin', methods=['POST', 'GET'])
+def signin():
+    try:
+        if request.method == 'GET':
+            return render_template('signin.html')
+        elif request.method == 'POST':
+           
+            _email = request.form['inputEmail']
+            _password = request.form['inputPassword']
+
+            if _email and _password:
+                con = sql.connect("user_db.db")
+                cur = con.cursor()
+                #cur.execute('SELECT SQLITE_VERSION()')
+                cur.execute("SELECT * FROM users WHERE email=?", (_email,))
+                data = cur.fetchone()
+                return render_template('welcome.html',username = _email)
+
+    except:
+        print("error")
+    finally:
+        print("signin Finally")
+
+
+def main():
+    app.run(host=HOST, port=PORT,debug=True)
+
+if __name__ == '__main__':
+    main()
+

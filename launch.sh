c6a8e562392fc691c641d2ad1aad750b3f5f42fb
Apoorv Mahajan <amahaja3@ncsu.edu>
2015-11-15 20:21:03 -0500

    Added M3 requirements

--- launch.sh
+++ launch.sh
@@ -2,5 +2,8 @@
 virtualenv env
 . env/bin/activate
 pip install flask
+pip install nose
+pip install coverage
+pip install pylint
 python -m py_compile app.py
 # triggering a post-build job on success
